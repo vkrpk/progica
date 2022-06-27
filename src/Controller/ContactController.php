167f8a96a94152bce56088c3d6d85c6b036a3b70
vkrpk <victor.krupka@orange.fr>
2022-06-27 14:30:49 +0200

    formulaire de contact avec mailer + recaptcha v3

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,18 +4,46 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
     {
         $contact = new Contact();
 
         $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+            $manager->persist($contact);
+            $manager->flush();
+
+            $email = (new TemplatedEmail())
+            ->from($contact->getEmail())
+            ->to('admin@progica.com')
+            ->subject($contact->getSubject())
+            // ->html($contact->getMessage());
+
+            ->htmlTemplate('emails/contact.html.twig')
+
+            // pass variables (name => value) to the template
+            ->context([
+                'contact' => $contact,
+            ]);
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('main');
+        }
 
         return $this->render('contact.html.twig', [
             'form' => $form->createView(),
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -11,12 +12,14 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            // ->setAction($this->generateUrl('contact'))
             ->add('fullname', TextType::class, [
                 'attr' => [
                     'class' => '',
@@ -69,6 +72,10 @@ class ContactType extends AbstractType
                 'constraints' => [
                     new Assert\NotBlank(),
                 ]
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
             ])
             // ->add('valider', SubmitType::class, [
             //     'attr' => [
